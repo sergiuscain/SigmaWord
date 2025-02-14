6e5baa69c5a8cfad7e001aa5123da65a440ca05e
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-02-14 23:19:12 +0300

    Ещё слова

--- SigmaWord/Services/VocabularyService.cs
+++ SigmaWord/Services/VocabularyService.cs
@@ -17,7 +17,7 @@ namespace SigmaWord.Services
         {
             List<FlashCard> flashcards = new List<FlashCard>();
             var assembly = Assembly.GetExecutingAssembly();
-            var resourceName = $"SigmaWord.Resources.Words.technology.json";
+            var resourceName = $"SigmaWord.Resources.Words.dailylife.json";
             var files = assembly.GetManifestResourceNames();
             using (Stream stream = assembly.GetManifestResourceStream(resourceName))
             {
