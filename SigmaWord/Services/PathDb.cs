a37a1ab44738bdfe4c9dba4c30a1ac8dd7b7c1f0
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-02-20 11:26:41 +0300

    Добавил получение патча базы данных для разных платформ

---
+++ SigmaWord/Services/PathDb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigmaWord.Services
+{
+    public static class PathDb
+    {
+        //var downloadPath = $"Filename={Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "SigmaWordDb.db")}";
+        public static string GetPath(string nameDb)
+        {
+            string pathDbSqlite = "";
+            if(DeviceInfo.Platform == DevicePlatform.Android)
+            {
+                pathDbSqlite = Path.Combine(FileSystem.AppDataDirectory, nameDb);
+                pathDbSqlite = $"Filename = {pathDbSqlite}";
+            }
+            else if(DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                pathDbSqlite = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                pathDbSqlite = Path.Combine(pathDbSqlite, "...", "Library", nameDb);
+            }
+            return pathDbSqlite;
+        }
+    }
+}
--- SigmaWord/Services/SigmaWordDbContext.cs
+++ SigmaWord/Services/SigmaWordDbContext.cs
@@ -12,13 +12,22 @@ namespace SigmaWord.Services
         public DbSet<FlashCard> FlashCards { get; set; }
         public DbSet<Category> Category { get; set; }
         public DbSet<ExampleSentence> ExampleSentence { get; set; }
+        public DbSet<DailyStatistics> DailyStatistics { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Настройка уникальности для DailyStatistics
+            modelBuilder.Entity<DailyStatistics>()
+                .HasIndex(d => d.Date)
+                .IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var dbPath = Path.Combine(FileSystem.AppDataDirectory, "SigmaWordDb.db");
-            var downloadPath = $"Filename={Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "SigmaWordDb.db")}";
-            var pathOnAndroid = $"Filename = {dbPath}";
-            optionsBuilder.UseSqlite(pathOnAndroid);
+
+            var pathDb = PathDb.GetPath("SigmaWordDb.db");
+            optionsBuilder.UseSqlite(pathDb);
         }
     }
 }
