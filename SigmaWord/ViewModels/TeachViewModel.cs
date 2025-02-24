83336c5ab19fcadc02f34f6475f77d3da5a63851
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-02-24 15:14:55 +0300

    Настроил график

--- SigmaWord/ViewModels/TeachViewModel.cs
+++ SigmaWord/ViewModels/TeachViewModel.cs
@@ -1,25 +1,52 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore;
-using System;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SigmaWord.ViewModels
 {
     public partial class TeachViewModel : ObservableObject
     {
+        public List<string> DaysOfWeek { get; set; }
+        public List<Axis> XAxes { get; set; }
+
+        public LineSeries<double> StudiedWordsSeries { get; set; }
+
+        public LineSeries<double> ReviewedWordsSeries { get; set; }
+
         public List<ISeries> Series { get; set; }
         public TeachViewModel()
         {
+            // Пример данных для графиков
+            DaysOfWeek = new List<string> { "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс" };
+
+            StudiedWordsSeries = new LineSeries<double>
+            {
+                Values = new double[] { 5, 5, 7, 5, 10, 5, 7 },
+                Name = "Изученные слова",
+                Stroke = new SolidColorPaint(SKColors.Red), // Задаем красный цвет для линии
+            };
+
+            ReviewedWordsSeries = new LineSeries<double>
+            {
+                Values = new double[] { 10, 15, 15, 10, 30, 15, 20 },
+                Name = "Повторенные слова",
+                Stroke = new SolidColorPaint(SKColors.Blue) // Задаем Синий цвет для линии
+            };
+
             Series = new List<ISeries>
             {
-                new LineSeries<double>
+                StudiedWordsSeries,
+                ReviewedWordsSeries
+            };
+            XAxes = new List<Axis>
+            {
+                new Axis
                 {
-                    Values = new double[] { 3, 5, 7, 4 }
+                    Labels = DaysOfWeek,
                 }
             };
         }
